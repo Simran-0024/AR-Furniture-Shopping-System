1f70239a193922651490af987df9c5e286fb9964
Simran-0024 <simranthecoder24@gmail.com>
2024-12-20 01:34:01 +0530

    Till Review 2
    Have Added the interface such as Login and Sign Up with javascript (Java) logic added in android studio

--- app/src/main/java/com/shoppingsystem/furnitureshopping/MainActivity.java
+++ app/src/main/java/com/shoppingsystem/furnitureshopping/MainActivity.java
@@ -1,7 +1,17 @@
 package com.shoppingsystem.furnitureshopping; // Replace with your actual package name
 
+import static android.content.ClipData.newIntent;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
@@ -11,11 +21,54 @@ import com.shoppingsystem.furnitureshopping.dao.FurnitureDao; // Import your Fur
 import com.shoppingsystem.furnitureshopping.database.FurnitureDatabase; // Import your FurnitureDatabase class
 
 public class MainActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    Button loginButton ;
+    TextView signUp;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        loginButton=findViewById(R.id.loginButton);
+        signUp = findViewById(R.id.signUp);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("simian") && password.getText().toString().equals("1234")){
+                    Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+
+                } else{
+                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (getApplicationContext(),MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+
+
 
         // Get the database instance and the DAO
         FurnitureDatabase db = FurnitureDatabase.getInstance(this); // Updated to use FurnitureDatabase
@@ -36,5 +89,10 @@ public class MainActivity extends AppCompatActivity {
                     ", Description: " + furniture.getDescription() +
                     ", Price: " + furniture.getPrice());
         }
+
+
+
     }
+
+
 }
